74a3ba3c72b199a8e8557af0ecf080ed0634965c
wahyuherlambang <57388791+wahyuherlambang@users.noreply.github.com>
2019-12-16 09:47:00 +0700

    Update unique-customer-per-brand-dev.sql

--- unique-customer-per-brand-dev.sql
+++ unique-customer-per-brand-dev.sql
@@ -1,27 +1,52 @@
 with 
-brand as (
-  select 'ai' as brand, date_trunc(date(issued_date), month) as issued_date_utc7, count(distinct(contact_email)) as unique_cust from `tvlk-realtime.airy.fact_flight_sales`
+overall_0 as (
+  select 'ai' as brand, date_trunc(date(issued_date), month) as issued_date_utc7, contact_email, ROW_NUMBER()
+				OVER(
+					PARTITION BY
+						contact_email
+					ORDER BY
+						issued_date
+				)
+				AS record_sequence from `tvlk-realtime.airy.fact_flight_sales`
   where issued_date is not null 
-    --and booking_status = 'ISSUED'
-    and issued_date >= '2017-12-01'
-    and issued_date <= '2018-12-31'
-  group by date_trunc(date(issued_date), month)
+    
 union all
 
-  select 'pg' as brand, date_trunc(date(issued_date), month) as issued_date_utc7, count(distinct(contact_email)) as unique_cust from `tvlk-realtime.pegipegi.flight_sales`
+  select 'pg' as brand, date_trunc(date(issued_date), month) as issued_date_utc7, contact_email, ROW_NUMBER()
+				OVER(
+					PARTITION BY
+						contact_email
+					ORDER BY
+						issued_date
+				)
+				AS record_sequence from `tvlk-realtime.pegipegi.flight_sales`
   where issued_date is not null 
-    --and booking_status = 'ISSUED'
-    and issued_date >= '2017-12-01'
-    and issued_date <= '2018-12-31'
-  group by date_trunc(date(issued_date), month)
+    
 union all
 
-  select 'tv' as brand, date_trunc(date(parse_datetime('%Y-%m-%d %H:%M:%E*S',issued_time)), month) as issued_date_utc7,count(distinct(contact_email)) as unique_cust from `tvlk-data-multibrand-prod.multibrand_flight.edw_fact_flight_booking_hashed`
-  where issued_time is not null 
-    --and booking_status = 'ISSUED'
-    and issued_time >= '2017-12-01'
-    and issued_time <= '2018-12-31'
-  group by date_trunc(date(parse_datetime('%Y-%m-%d %H:%M:%E*S',issued_time)), month)
+  select 'tv' as brand, date_trunc(date(parse_datetime('%Y-%m-%d %H:%M:%E*S',issued_time)), month) as issued_date_utc7, contact_email, ROW_NUMBER()
+				OVER(
+					PARTITION BY
+						contact_email
+					ORDER BY
+						date(parse_datetime('%Y-%m-%d %H:%M:%E*S',issued_time))
+				)
+				AS record_sequence from `tvlk-data-multibrand-prod.multibrand_flight.edw_fact_flight_booking_hashed` a
+        join `tvlk-data-multibrand-prod.multibrand_flight.DIM_AIRPORT` b on b.string_field_0 = a.source_airport_id
+  where issued_time is not null and b.string_field_2 = 'ID' and b.string_field_2 = 'ID' 
 )
-select * from brand
-       
+-- ,
+-- overall_1 as (
+-- -- check whether multi-brand or not.
+-- select contact_email, count(distinct brand) as unique_brand  from overall_0
+-- group by contact_email
+-- order by count(distinct brand) desc
+-- )
+
+select brand, issued_date_utc7, count( distinct contact_email) from overall_0
+group by brand, issued_date_utc7
+
+
+-- -- check which brand is being used.
+-- select distinct brand from overall_0
+-- where contact_email='XhUqVTDk/0zglyDWp4aqAx0pAPxZ3cUQT29DLZtcxtJQoifNit5OiF3ua9bnzWHr+jby6I5XLqEpmcWf43LrMQ=='
