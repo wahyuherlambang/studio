6de0d596d490d249c2658d9fe94c505d7f343722
wahyuherlambang <57388791+wahyuherlambang@users.noreply.github.com>
2020-02-11 13:36:44 +0700

    Update monthly_transacting_user.sql

--- investor-metrics/monthly_transacting_user.sql
+++ investor-metrics/monthly_transacting_user.sql
@@ -1,7 +1,8 @@
 select 
 DATE( TIMESTAMP_TRUNC( TIMESTAMP_ADD( TIMESTAMP_MILLIS( last_payment_assignment_timestamp ), INTERVAL 7 HOUR ), MONTH ) ) AS transaction_month,
 SUBSTR(locale,-2) as country,
+product,
 count(distinct(profile_id)) as unique_trx_user
 from `tvlk-data-mkt-prod.datamart.sales_table`
 where profile_id IS NOT NULL AND booking_status = 'ISSUED' 
-group by 1,2
+group by 1,2,3
